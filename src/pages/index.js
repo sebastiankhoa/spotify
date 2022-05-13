0a9e3d421a5b6ce5b86f8a2cac67ffdc2d4c6787
Khoa <khoa@Khoas-MacBook-Pro.local>
2022-05-13 14:24:37 +0700

    change index

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 	const { status, data: session } = useSession({
 		required: true,
 		onUnauthenticated() {
-			router.push("/auth/signin");
+			router.push("/");
 		},
 	});
 
